cdcf240d8f535a6c4e489dde7c10eefc98c4fd54
Septias <39526136+Septias@users.noreply.github.com>
2020-12-23 15:36:55 +0100

    shorten req url

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ use ron::{
 };
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
-use std::{fs::File, io::Write, path::PathBuf, sync::Arc};
+use std::{
+    fs::File,
+    io::{ErrorKind, Write},
+    path::PathBuf,
+    sync::Arc,
+};
 use tokio::{fs::create_dir, task};
 
 #[tokio::main]
@@ -53,9 +58,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         }
         ilias_tree
     };
-    
-    sync(ilias_tree, PathBuf::new()).await;
-    println!("{:?}", ilias_tree);
+    // don't fkn drop ilias pls
+    let ilias_tree = Box::leak(Box::new(ilias_tree));
+    sync(ilias_tree, PathBuf::new()).await?;
     Ok(())
 }
 
@@ -69,26 +74,37 @@ fn set_ids(node: &mut IlNode, id: &mut u16) {
     }
 }
 
-
 fn sync(node: &'static IlNode, mut path: PathBuf) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
+        let mut handles = vec![];
         match node.breed {
             IlNodeType::Forum => {}
             IlNodeType::Folder => {
                 path.push(&node.title);
-                create_dir(&path);
+                match create_dir(&path).await {
+                    Ok(_) => {
+                        info!("created Folder {}", &node.title)
+                    }
+                    Err(err) if err.kind() == ErrorKind::AlreadyExists => {}
+                    Err(err) => {
+                        error!("couldn't create folder \"{}\" - {}", &node.title, err)
+                    }
+                }
+
                 if let Some(children) = &node.children {
                     for child in children {
-                        sync(child, path.clone());
+                        handles.push(sync(child, path.clone()));
                     }
                 }
             }
             IlNodeType::DirectLink => {}
             IlNodeType::File => {}
         }
+        for handle in handles {
+            handle.await.unwrap();
+        }
     })
 }
-
 
 async fn request_il_page(
     uri: &str,
@@ -97,21 +113,12 @@ async fn request_il_page(
     let req = Request::builder()
         .method(Method::GET)
         .uri("https://ilias.uni-freiburg.de/".to_owned() + &uri)
-        .header("authority", "ilias.uni-freiburg.de")
-        .header("upgrade-insecure-requests", 1)
-        .header("dnt", 1)
-        .header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-        .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-        .header("sec-fetch-site", "same-origin")
-        .header("sec-fetch-mode", "navigate")
-        .header("sec-fetch-user", "?1")
-        .header("sec-fetch-dest", "document")
-        .header("referer", "https://ilias.uni-freiburg.de/ilias.php?baseClass=ilPersonalDesktopGUI&cmd=jumpToSelectedItems")
-        .header("accept-language", "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7")
-        .header("cookie", "iom_consent=00000000000000&1604408733754; ioam2018=000ef5c0cec6382585fa1559e:1632834334057:1604408734057:.uni-freiburg.de:6:ak025:dbs:noevent:1604871141009:nb8xt2; ilClientId=unifreiburg; _shibsession_64656661756c7468747470733a2f2f696c6961732e756e692d66726569627572672e64652f73686962626f6c657468=_0ed19f012091e81c7f66970654a4def0; PHPSESSID=qrb2h55lg6hh17cn9ckmnpiid0")
-        .body(Body::empty()).unwrap();
+        .header("cookie", "PHPSESSID=qrb2h55lg6hh17cn9ckmnpiid0")
+        .body(Body::empty())
+        .unwrap();
 
     let mut resp = client.request(req).await?;
+    info!("status: {}", resp.status());
     let mut bytes = vec![];
     while let Some(chunk) = resp.body_mut().data().await {
         let chunk = chunk?;
@@ -131,7 +138,7 @@ async fn get_child_pages(
     for element in elements {
         element_infos.push((
             element.value().attr("href").unwrap().to_string(),
-            element.inner_html(),
+            element.inner_html().replace("/", " "),
         ))
     }
     element_infos
