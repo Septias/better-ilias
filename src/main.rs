eae33958b4b7a1759cf67fd1713f8261732b70b3
Septias <39526136+Septias@users.noreply.github.com>
2020-12-31 10:12:14 +0100

    remove file-work in recur-sync

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Utc};
 use futures::future::join_all;
 use hyper::{Body, Client, Method, Request, StatusCode, body::HttpBody as _, client::HttpConnector};
 use hyper_tls::HttpsConnector;
 use log::{error, info};
-use ron::{
-    de::from_reader,
-    ser::{to_string_pretty, PrettyConfig},
-};
+use ron::{de::from_bytes, from_str, ser::{to_string_pretty, PrettyConfig}};
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, fs::File, io::{ErrorKind, Write}, path::PathBuf, sync::{Arc, Mutex}, unimplemented};
-use tokio::{fs::create_dir, task::{self, JoinHandle}};
+use std::{collections::HashMap, io::ErrorKind, path::PathBuf, sync::{Arc, Mutex}, unimplemented};
+use tokio::{fs::{File, create_dir}, io::{AsyncReadExt, AsyncWriteExt}, task::{self, JoinHandle}};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
+    let https = HttpsConnector::new();
+    let client = Arc::new(Client::builder().build::<_, hyper::Body>(https));
+    let file_watcher = Arc::new(FileWatcher::new());
 
-    let client = Arc::new(Client::builder().build::<_, hyper::Body>(HttpsConnector::new()));
-
-    let ilias_tree: IlNode = if let Some(ilias_tree) = match File::open("structure.ron") {
-        Ok(save) => {
-            if let Ok(ilias_tree) = from_reader(save) {
+    let ilias_tree: IlNode = if let Some(ilias_tree) = match File::open("structure.ron").await {
+        Ok(mut save) => {
+            let mut buffer = vec![];
+            save.read_to_end(&mut buffer).await?;
+            if let Ok(ilias_tree) = from_bytes(&buffer) {
                 Some(ilias_tree)
             } else {
                 None
@@ -37,6 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .to_string(),
             "Rechnernetze".to_string(),
             client.clone(),
+            file_watcher.clone()
         )
         .await?.expect("couldn't fetch Ilias-tree");
 
@@ -46,21 +47,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let pretty = PrettyConfig::new()
             .with_separate_tuple_members(true)
             .with_enumerate_arrays(true);
-        let mut writer = File::create("structure.ron").expect("unable to create save-file");
+        let mut writer = File::create("structure.ron").await.expect("unable to create save-file");
         let s = to_string_pretty(&ilias_tree, pretty).unwrap();
-        let write_result = writer.write_all(s.as_bytes());
+        let write_result = writer.write_all(s.as_bytes()).await;
         if let Err(_) = write_result {
             error!("Can't save structure.ron");
         }
         ilias_tree
     };
-    // don't fkn drop ilias pls
-    let ilias_tree = Box::leak(Box::new(ilias_tree));
 
-    let file_watcher = Arc::new(Mutex::new(FileWatcher::new()));
-    
-    // sync ilias_tree to local files
-    sync(ilias_tree, PathBuf::new(), client.clone(), file_watcher).await?;
+    let ilias_tree = Box::leak(Box::new(ilias_tree));
+    sync(ilias_tree, PathBuf::new(), client.clone()).await?;
+
     Ok(())
 }
 
@@ -76,94 +74,48 @@ fn set_ids(node: &mut IlNode, id: &mut u16) {
 
 struct Config();
 
-impl Config{
+impl Config {
     fn get_token() -> &'static str {
-        "rtu7t6p8v9o4ucov79i0vv0rhi"
+        "ffv5qmc43tviq8o0941f67cj4b"
     }
 }
 struct FileWatcher {
-    files: HashMap<String, FileInfo>
+    files: Mutex<HashMap<String, FileInfo>>,
 }
 
 impl FileWatcher {
-    fn add_file(&mut self, file_info: FileInfo) -> JoinHandle<()> {
-        let join_handle = FileWatcher::download_file(&file_info.uri, &file_info.path, &file_info.ending);
-        self.files.insert(file_info.uri.to_string(), file_info);
-        join_handle
+    fn add_file(&mut self, file_info: FileInfo){
+        self.files.lock().unwrap().insert(file_info.uri.to_string(), file_info);
     }
     fn download_file(uri: &str, path: &PathBuf, ending: &str) -> JoinHandle<()> {
-        tokio::spawn( async move {
-            unimplemented!()
-        })
+        tokio::spawn(async move { unimplemented!() })
     }
     fn new() -> Self {
         return {
-            FileWatcher{
-                files: HashMap::new()
+            FileWatcher {
+                files: Mutex::new(HashMap::new()),
             }
-        }
-    }
-    fn process(&mut self, file_info: FileInfo) -> Option<JoinHandle<()>>{
-        let last_changed = self.files.get(&file_info.uri);
-
-        match last_changed {
-            Some(file) => None,
-            None => Some(self.add_file(file_info))
-        }
+        };
     }
 }
 
-struct FileInfo{
+struct FileInfo {
     ending: String,
     uri: String,
     date: DateTime<Utc>,
     path: PathBuf,
 }
 
-
-fn get_file_info(html: Arc<Html>, path: PathBuf, uri: &str) -> Vec<FileInfo>{
-    let containers = Selector::parse(".ilContainerListItemOuter .il_ContainerItemTitle a").unwrap();
-    let pdf_selector = Selector::parse(".").unwrap();
-    let elements = html.select(&containers);
-    let mut file_infos = vec![];
-    for element in elements {
-        let mut path = path.clone();
-        path.push( element.inner_html().replace("/", " "));
-        let el_type = element.select(&pdf_selector).last().unwrap().inner_html();
-        file_infos.push(FileInfo{
-            uri: element.value().attr("href").unwrap().to_owned(),
-            path: path,
-            date: Utc::now(),
-            ending: el_type
-        })
-    }
-    file_infos
-}
-
 fn sync(
     node: &'static IlNode,
     mut path: PathBuf,
     client: Arc<Client<HttpsConnector<HttpConnector>>>,
-    file_watcher: Arc<Mutex<FileWatcher>>
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         let mut sync_handles = vec![];
-        let mut file_handles = vec![];
         match node.breed {
             IlNodeType::Folder => {
                 path.push(&node.title);
-                if node.sync {
-                    
-                        let html = Arc::new(request_il_page(&node.uri, client.clone()).await.unwrap());
-                        let mut file_watcher = file_watcher.lock().unwrap();
-             
-                        for file in get_file_info(html.clone(), path.clone(), &node.uri){
-                            if let Some(handle) = file_watcher.process(file){
-                                file_handles.push(handle);
-                            }
-                        }
-                    
-                }
                 match create_dir(&path).await {
                     Ok(_) => {
                         info!("created Folder {}", &node.title)
@@ -173,20 +125,18 @@ fn sync(
                         error!("couldn't create folder \"{}\" - {}", &node.title, err)
                     }
                 }
-
                 if let Some(children) = &node.children {
                     for child in children
                         .iter()
                         .filter(|child| child.breed == IlNodeType::Folder)
                     {
-                        sync_handles.push(sync(child, path.clone(), client.clone(), file_watcher.clone()));
+                        sync_handles.push(sync(child, path.clone(), client.clone()));
                     }
                 }
             }
             _ => (),
         }
         join_all(sync_handles).await;
-        join_all(file_handles).await;
     })
 }
 
@@ -201,8 +151,12 @@ async fn request_il_page(
         .body(Body::empty())
         .unwrap();
     let mut resp = client.request(req).await?;
-    if resp.status() != StatusCode::OK{
-        error!("{} Problem with requestion ilias-page \" {}\"", resp.status(), uri);
+    if resp.status() != StatusCode::OK {
+        error!(
+            "{} Problem with requestion ilias-page \" {}\"",
+            resp.status(),
+            uri
+        );
     }
     let mut bytes = vec![];
     while let Some(chunk) = resp.body_mut().data().await {
@@ -221,13 +175,12 @@ async fn get_child_pages(
     uri: &str,
     client: Arc<Client<HttpsConnector<HttpConnector>>>,
 ) -> Vec<PageInfo> {
-
     let containers = Selector::parse(".ilContainerListItemOuter .il_ContainerItemTitle a").unwrap();
     let html = request_il_page(uri, client).await.unwrap();
     let elements = html.select(&containers);
     let mut element_infos = vec![];
     for element in elements {
-        element_infos.push(PageInfo{
+        element_infos.push(PageInfo {
             uri: element.value().attr("href").unwrap().to_string(),
             title: element.inner_html().replace("/", " "),
         })
@@ -276,6 +229,7 @@ fn get_ilias_tree(
     uri: String,
     title: String,
     client: Arc<Client<HttpsConnector<HttpConnector>>>,
+    file_watcher: Arc<FileWatcher>
 ) -> JoinHandle<Option<IlNode>> {
     task::spawn(async move {
         if let Some(node_type) = get_il_node_type(&uri) {
@@ -297,7 +251,7 @@ fn get_ilias_tree(
                     // create children
                     let mut handles = vec![];
                     for element in child_elements {
-                        handles.push(get_ilias_tree(element.uri, element.title, client.clone()));
+                        handles.push(get_ilias_tree(element.uri, element.title, client.clone(), file_watcher.clone()));
                     }
                     // load children and add them to the node
                     let mut children = vec![];
