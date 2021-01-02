c388308406d5425afa7fa43ce2a9d6ec7aced4bc
Septias <39526136+Septias@users.noreply.github.com>
2021-01-02 16:03:48 +0100

    first sync-method for File-Watcher

--- src/sync.rs
+++ src/sync.rs
@@ -72,6 +72,36 @@ impl FileWatcher {
     fn download_file(_uri: &str, _path: &PathBuf, _ending: &str) -> JoinHandle<()> {
         tokio::spawn(async move { unimplemented!() })
     }
+    pub async fn sync(
+        &mut self,
+        node_tree: &IlNode,
+        client: Arc<Client<HttpsConnector<HttpConnector>>>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let files = self.files.lock().unwrap();
+        let mut watched = files.clone();
+        while watched.len() > 0 {
+            let elem = get_node(node_tree, watched.pop().unwrap()).unwrap().parent;
+            println!("{}", elem);
+            if let Some(parent) = get_node(node_tree, elem) {
+                let html = request_il_page(&parent.uri, client.clone()).await?;
+                let versions = get_versions(&html);
+                for version_info in versions {
+                    let child_node = get_node(node_tree, *self.title_id_map.lock().unwrap().get(&version_info.title).unwrap()).unwrap();
+                    if &version_info.version > &child_node.id{
+                        if let Some(sync) = &child_node.sync {
+                            FileWatcher::download_file(&child_node.uri, &sync.path);
+                        } else {
+                            warn!("No sync Info for file {}", child_node.title);
+                        }
+                        
+                    }
+                    watched.remove(child_node.id as usize);
+                }
+            }
+        } 
+        Ok(()) // ot
+    }
+
     pub fn new() -> Self {
         return {
             FileWatcher {
