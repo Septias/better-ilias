f4995a047a103af2653fbd140622cadc96fb26fe
Septias <39526136+Septias@users.noreply.github.com>
2021-01-06 18:24:18 +0100

    add rocket to project and create api-endpoint

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,9 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 use log::info;
-use std::sync::Arc;
+use rocket::State;
+use rocket_contrib::json::Json;
+use std::sync::{Arc, Mutex};
 use sync::{FileSelect, FileWatcher};
-use tree::get_or_create_ilias_tree;
+use tree::{IlNode, get_or_create_ilias_tree};
+#[macro_use] extern crate rocket;
+
 mod config;
 mod helpers;
 mod sync;
@@ -11,24 +16,31 @@ mod tree;
 
 pub type IdSize = u16;
 
+#[get("/")]
+fn index(node: State<Arc<Mutex<IlNode>>>) -> Json<IlNode> {
+    let node = node.lock().unwrap();
+    Json(node.clone())
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main(){
     env_logger::init();
     let https = HttpsConnector::new();
     let client = Arc::new(Client::builder().build::<_, hyper::Body>(https));
 
     let mut file_watcher = FileWatcher::new();
 
-    let ilias_tree = get_or_create_ilias_tree(client.clone(), &mut file_watcher).await?;
-
+    let ilias_tree = get_or_create_ilias_tree(client.clone(), &mut file_watcher).await.unwrap();
+    
     info!("sync structure to local filessystem");
 
-    sync::sync(ilias_tree.clone(), client.clone()).await?;
+    //sync::sync(ilias_tree.clone(), client.clone()).await?;
 
     info!("sync files");
     //add_to_file_watcher(&ilias_tree.lock().unwrap(), &mut file_watcher, "Bischte Dumm".to_string()); //remove
-    file_watcher
+    /* file_watcher
         .sync(ilias_tree, FileSelect::All, client.clone())
-        .await?;
-    Ok(())
+        .await?;  */
+   
+    rocket::ignite().mount("/", routes![index]).manage(ilias_tree.clone()).launch();
 }
