543463c2407ad341a08dbca78d60ad5684931eb5
Septias <39526136+Septias@users.noreply.github.com>
2021-01-10 20:22:00 +0100

    added ability to hide folders

--- Frontend/src/components/Folder.vue
+++ Frontend/src/components/Folder.vue
@@ -1,52 +1,59 @@
 <template >
-  <svg
-    focusable="false"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    class="text-accent hover:text-white fill-current inline"
-  >
-    <template v-if="node.children.length">
-      <path
-        v-if="expanded"
-        d="M5.843 9.593L11.5 15.25l5.657-5.657l-.707-.707l-4.95 4.95l-4.95-4.95l-.707.707z"
-        fill="currentColor"
-      ></path>
-      <path
-        v-else
-        d="M8.593 18.157L14.25 12.5L8.593 6.843l-.707.707l4.95 4.95l-4.95 4.95l.707.707z"
-        fill="currentColor"
-      ></path>
-    </template>
-    <rect
-      v-else
-      x="9"
-      y="9"
-      width="5"
-      height="5"
-      style="fill: rgba(0, 0, 0, 0); stroke-width: 2; stroke: currentColor"
-    />
-  </svg>
-
-  <span class="p-1 rounded-sm hover:bg-accent" @click="expanded = !expanded">{{
-    node.title
-  }}</span>
-  <ul class="node_tree" :class="{ shrinked: !expanded }">
-    <li
-      v-for="(child, index) in node.children"
-      :key="child.id"
-      :class="[child.breed, 'node_tree_item']"
-      :index="index"
+  <span v-if="node.visible || edit_visibility">
+    <SelectBox v-if="edit_visibility" :ckecked="node.visibile"></SelectBox>
+    <svg
+      focusable="false"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      class="text-accent hover:text-white fill-current inline"
+      @click="expanded = !expanded"
     >
-      <component
+      <template v-if="node.children.length">
+        <path
+          v-if="expanded"
+          d="M5.843 9.593L11.5 15.25l5.657-5.657l-.707-.707l-4.95 4.95l-4.95-4.95l-.707.707z"
+          fill="currentColor"
+        ></path>
+        <path
+          v-else
+          d="M8.593 18.157L14.25 12.5L8.593 6.843l-.707.707l4.95 4.95l-4.95 4.95l.707.707z"
+          fill="currentColor"
+        ></path>
+      </template>
+      <rect
+        v-else
+        x="9"
+        y="9"
+        width="5"
+        height="5"
+        style="fill: rgba(0, 0, 0, 0); stroke-width: 2; stroke: currentColor"
+      />
+    </svg>
+
+    <span
+      class="p-1 rounded-sm hover:bg-accent text-white"
+      :class="{ 'text-opacity-25': !node.visible && edit_visibility }"
+      @click="handle_click"
+      >{{ node.title }}</span
+    >
+    <ul class="node_tree" :class="{ shrinked: !expanded }">
+      <li
+        v-for="(child, index) in node.children"
+        :key="child.id"
+        :class="[child.breed, 'node_tree_item']"
         :index="index"
-        :is="child.breed"
-        :node="child"
-        @set_invisible="handle_set_inivisible"
-        @set_visible="handle_set_visible"
-      ></component>
-    </li>
-  </ul>
+      >
+        <component
+          :index="index"
+          :is="child.breed"
+          :node="child"
+          @set_invisible="handle_set_inivisible"
+          @set_visible="handle_set_visible"
+        ></component>
+      </li>
+    </ul>
+  </span>
 </template>
 
 <script lang="ts">
@@ -54,6 +61,7 @@ import { ref, defineComponent } from "vue";
 import File from "./File.vue";
 import DirectLink from "./DirectLink.vue";
 import Forum from "./Forum.vue";
+import { useVisibility } from "./compositions";
 
 export default defineComponent({
   components: { File, DirectLink, Forum },
@@ -73,9 +81,21 @@ export default defineComponent({
       this.$emit("set_visible", path);
     },
   },
-  setup() {
+  setup(props, { emit }) {
     let expanded = ref(true);
-    return { expanded };
+    let { edit_visibility } = useVisibility();
+    function handle_click() {
+      if (edit_visibility.value) {
+        if (props.node.visible) {
+          emit("set_invisible", [props.index]);
+        } else {
+          emit("set_visible", [props.index]);
+        }
+      } else {
+        expanded.value = !expanded.value;
+      }
+    }
+    return { expanded, edit_visibility, handle_click };
   },
 });
 </script>
