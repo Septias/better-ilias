c4f0f4e9f1d6aa9284bb43b377a68e5523db9c08
Hocuri <hocuri@gmx.de>
2021-04-26 10:55:38 +0200

    Make build.sh work on Linux (there is no .exe ending on Linux)

--- build.sh
+++ build.sh
@@ -13,7 +13,12 @@ folder=builds/build_$(date +%s)
 mkdir -p $folder
 
 cp -r Frontend/dist $folder
-cp Backend/Rocket.toml Backend/target/release/better_ilias.exe $folder
+cp Backend/Rocket.toml $folder
+cp Backend/target/release/better_ilias.exe $folder || cp Backend/target/release/better_ilias $folder
+
+echo
+echo "Finished successfully."
+echo
 
 #just for me
 cp -r $folder/* /d/dev/Installations/BetterIlias
